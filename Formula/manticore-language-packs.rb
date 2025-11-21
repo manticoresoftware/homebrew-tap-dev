2b7215f8c80e83db22ac9fe79629fa7b721e552a
Sergey Nikolaev <prostuda@academ.org>
2025-11-21 20:57:36 +0700

    added jieba

--- Formula/manticore-language-packs.rb
+++ Formula/manticore-language-packs.rb
@@ -5,11 +5,27 @@ class ManticoreLanguagePacks < Formula
   version "0.9.9"
 
   def install
-    (share/"manticore").mkpath
+    manticore_share = share/"manticore"
+    manticore_share.mkpath
 
-    langs = %w[de en ru]
-    langs.each do |lang|
-      system "curl -sSL https://repo.manticoresearch.com/repository/morphology/#{lang}.pak.tgz | tar xvzf - -C #{share/"manticore"}"
+    # Download and unpack morphology .pak files
+    %w[de en ru].each do |lang|
+      archive = "#{lang}.pak.tgz"
+      system "curl", "-sSL",
+             "https://repo.manticoresearch.com/repository/morphology/#{lang}.pak.tgz",
+             "-o", archive
+      system "tar", "xvzf", archive, "-C", manticore_share.to_s
     end
+
+    # Download jieba dict files into the same directory as the .pak files
+    %w[hmm_model idf stop_words user.dict].each do |name|
+      system "curl", "-sSL",
+             "https://raw.githubusercontent.com/manticoresoftware/cppjieba/master/dict/#{name}.utf8",
+             "-o", (manticore_share/"#{name}.utf8")
+    end
+
+    system "curl", "-sSL",
+           "https://raw.githubusercontent.com/manticoresoftware/jieba/refs/heads/master/extra_dict/dict.txt.big",
+           "-o", (manticore_share/"jieba.dict.utf8")
   end
 end
